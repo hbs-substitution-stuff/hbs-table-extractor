e3ed7502545ac5d25bbd62b859d453dcee27eb1c
xUnixorn <jonas.crull@gmx.de>
2021-10-11 01:15:07 +0200

    began combining line segments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use lopdf::Document;
 use encoding_rs::{WINDOWS_1252, Decoder, DecoderResult};
 use std::error::Error;
+use std::collections::HashMap;
 
 fn main() {
     let pdf = Document::load("./VertretungsplanA4_Dienstag.pdf").unwrap();
@@ -39,112 +40,89 @@ fn main() {
     current.decompress();
     let current = current.decode_content().unwrap();
 
-    let date_bytes = current.operations
-        .iter()
-        .filter(|o| o.operator == "Tj")
-        .nth(2)
-        .unwrap()
-        .operands[0]
-        .as_str()
-        .unwrap();
+    // let date_bytes = current.operations
+    //     .iter()
+    //     .filter(|o| o.operator == "Tj")
+    //     .nth(2)
+    //     .unwrap()
+    //     .operands[0]
+    //     .as_str()
+    //     .unwrap();
+    //
+    // let date = WINDOWS_1252.decode(date_bytes).0;
 
-    let date = WINDOWS_1252.decode(date_bytes).0;
-
-    let mut objects = Vec::new();
+    let mut texts = Vec::new();
+    let mut horizontal_lines = Vec::new();
+    let mut vertical_lines = Vec::new();
 
     for (i, op) in current.operations.iter().enumerate() {
-        if op.operator == "Tj" {
-            let text = WINDOWS_1252.decode(op.operands[0].as_str().unwrap()).0;
-            let td = &current.operations[i - 1].operands;
+        match op.operator.as_str() {
+            "Tj" => {
+                let td = &current.operations[i - 1];
 
-            let position = Point::new(td[0].as_f64().unwrap(), td[1].as_f64().unwrap());
+                if td.operator == "Td" {
+                    let td_ops = &td.operands;
+                    let tj_ops = &op.operands;
 
-            objects.push(Objects::Text(Text::new(text.parse().unwrap(), position)))
+                    let text = WINDOWS_1252.decode(tj_ops[0].as_str().unwrap()).0
+                        .parse()
+                        .unwrap();
+
+                    let position = Point::new((td_ops[0].as_f64().unwrap() as i64), (td_ops[1].as_f64().unwrap() as i64));
+
+                    texts.push(Text::new(text, position))
+                } else {
+                    println!("{}", "ERROR: Td expected before Tj");
+                }
+            },
+            "l" => {
+                let m = &current.operations[i - 1];
+
+                if m.operator == "m" {
+                    let m_ops = &m.operands;
+                    let l_ops = &op.operands;
+
+                    let start = Point::new((m_ops[0].as_f64().unwrap() as i64), (m_ops[1].as_f64().unwrap() as i64));
+
+                    let end = Point::new((l_ops[0].as_f64().unwrap() as i64), (l_ops[1].as_f64().unwrap() as i64));
+
+                    let array = if (start.y - end.y as i64) == 0 {
+                        &mut horizontal_lines
+                    } else if (start.x - end.x) == 0 {
+                        &mut vertical_lines
+                    } else {
+                        panic!("{}", "ERROR: line is diagonal")
+                    };
+
+                    array.push(Line::new(start, end));
+                } else {
+                    println!("{}", "ERROR: m expected before l");
+                }
+            },
+            _ => (),
         }
+    }
 
-        if op.operator == "l" {
-            let m = &current.operations[i - 1].operands;
-            let start = Point::new(m[0].as_f64().unwrap(), m[1].as_f64().unwrap());
+    let mut start_to_end = HashMap::new();
 
-            let l = &op.operands;
-            let end = Point::new(l[0].as_f64().unwrap(), l[1].as_f64().unwrap());
+    for line in horizontal_lines {
+        start_to_end.insert(line.0, line.1);
+    }
 
-            objects.push(Objects::Line(Line::new(start, end)));
+    let mut to_remove = Vec::new();
+
+    for start in start_to_end.keys() {
+        let end = start_to_end.get(start).unwrap();
+        if let Some(new_end) = start_to_end.get(end) {
+            to_remove.push(end);
+
+            let point = start_to_end.get_mut(start).unwrap();
+            point.x = new_end.x;
+            point.y = new_end.y;
         }
     }
 
-    // for ob in objects {
-    // 	match ob {
-    // 		Objects::Text(t) => println!("Text: {}, ({}, {})", t.text, t.position.x, t.position.y),
-    // 		Objects::Line(l) => {
-    // 			if ((l.0.x as i64) - (l.1.x as i64)) == 0 {
-    // 				println!("Vertical Line: ({}, {}), ({}, {})", l.0.x, l.0.y, l.1.x, l.1.y);
-    // 			} else if ((l.0.y as i64) - (l.1.y as i64)) == 0 {
-    // 				println!("Horizontal Line: ({}, {}), ({}, {})", l.0.x, l.0.y, l.1.x, l.1.y);
-    // 			} else {
-    // 				println!("Diagonal Line: ({}, {}), ({}, {})", l.0.x, l.0.y, l.1.x, l.1.y);
-    // 			}
-    // 		},
-    // 	}
-    // }
-
-    print!("{}", "x = [");
-    for ob in &objects {
-        match ob {
-            Objects::Text(t) => {
-                print!("{}, ", t.position.x)
-            },
-            Objects::Line(l) => {
-                print!("{}, ", l.0.x)
-            },
-        }
-    }
-    print!("{}", "]");
-
-    println!("{}", "");
-
-    print!("{}", "y = [");
-    for ob in &objects {
-        match ob {
-            Objects::Text(t) => {
-                print!("{}, ", t.position.y)
-            },
-            Objects::Line(l) => {
-                print!("{}, ", l.0.y)
-            },
-        }
-    }
-    print!("{}", "]");
-
-    println!("{}", "");
-
-    print!("{}", "u = [");
-    for ob in &objects {
-        match ob {
-            Objects::Text(t) => {
-                print!("{}, ", 1)
-            },
-            Objects::Line(l) => {
-                print!("{}, ", l.1.x - l.0.x)
-            },
-        }
-    }
-    print!("{}", "]");
-
-    println!("{}", "");
-
-    print!("{}", "v = [");
-    for ob in &objects {
-        match ob {
-            Objects::Text(t) => {
-                print!("{}, ", 1)
-            },
-            Objects::Line(l) => {
-                print!("{}, ", l.1.y - l.0.y)
-            },
-        }
-    }
-    print!("{}", "]");
+    //TODO remove all with to_remove
 }
 
 struct Text {
@@ -167,23 +145,23 @@ impl Line {
     fn new(p1: Point, p2: Point) -> Self {
         Self(p1, p2)
     }
+
+    fn set_end(&mut self, p1: Point) {
+        self.1 = p1;
+    }
 }
 
+#[derive(Eq, PartialEq, Hash, Clone)]
 struct Point {
-    x: f64,
-    y: f64,
+    x: i64,
+    y: i64,
 }
 
 impl Point {
-    fn new(x: f64, y: f64) -> Self {
+    fn new(x: i64, y: i64) -> Self {
         Self {
             x,
             y,
         }
     }
-}
-
-enum Objects {
-    Text(Text),
-    Line(Line),
 }
